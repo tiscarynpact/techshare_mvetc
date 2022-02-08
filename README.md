cebd84f560326397e40cbbcd5fa070a774458592
tiscarynpact <sebastien.tiscar+git@ynpact.com>
2022-02-08 17:00:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-qrrq32
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qrrq32)
---
+++ mvc.js
@@ -0,0 +1,26 @@
+class Controler {
+  model = null;
+  add(value){
+    this.model.add(value)
+  }
+  constructor(model) {
+    this.model = model;
+  }
+}
+
+class Model {
+  listeners = [];
+  constructor(startCount) {
+    this.count = startCount;
+  }
+  add(num) {
+    this.count += Number(num);
+    this.notify();
+  }
+  addListener(listener) {
+    this.listeners = [...this.listeners, listener];
+  }
+  notify() {
+    this.listeners.forEach((listener) => listener(this.count));
+  }
+}
